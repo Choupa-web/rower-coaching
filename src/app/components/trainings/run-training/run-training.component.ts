bd054f86a6e1a5681e32678b828cd10927505b08
Choupa-web <persan78@hotmail.com>
2023-08-15 19:40:29 +0200

    ROW-6 mecanisme d'enchainement des étapes

--- src/app/components/trainings/run-training/run-training.component.ts
+++ src/app/components/trainings/run-training/run-training.component.ts
@@ -1,6 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, DestroyRef, OnInit, inject} from '@angular/core';
 import {IStage, ITraining} from '../../../models/training.model';
-import {BehaviorSubject} from 'rxjs';
+import {BehaviorSubject, take, timer} from 'rxjs';
 import {RunTraining} from '../../../models/training-iterator.model';
 
 @Component({
@@ -9,10 +9,11 @@ import {RunTraining} from '../../../models/training-iterator.model';
   styleUrls: ['./run-training.component.scss']
 })
 export class RunTrainingComponent implements OnInit {
+  private destroyRef = inject(DestroyRef);
   rowerTraining: ITraining | undefined;
-  stage: IStage | undefined;
+  currentStage: IStage | undefined;
   trainingIterator$: BehaviorSubject<IStage | undefined> = new BehaviorSubject<IStage | undefined>(undefined);
-  onGoingTraining: RunTraining | undefined;
+  onGoingTraining: RunTraining<IStage> | undefined;
 
   ngOnInit() {
     this.rowerTraining = {
@@ -30,19 +31,38 @@ export class RunTrainingComponent implements OnInit {
         }
       ]
     };
-    this.trainingIterator$.subscribe({
+    /*this.trainingIterator$.subscribe({
       next: onGoingStage => {
         console.log("onGoingStage: ",onGoingStage);
         if(onGoingStage) {
-          this.stage = onGoingStage;
+          this.currentStage = onGoingStage;
           if (this.onGoingTraining?.hasNext()) {
             this.trainingIterator$.next(this.onGoingTraining?.next());
           }
         }
       }
-    });
-    this.onGoingTraining = new RunTraining(this.rowerTraining);
-    this.trainingIterator$.next(this.onGoingTraining.first());
+    });*/
+    this.onGoingTraining = new RunTraining<IStage>(this.rowerTraining.stages);
+   
+
+    do {
+      this.currentStage = this.onGoingTraining.item;
+      const delay = 60/this.currentStage.cadence*1000;
+      console.log('delay', delay);
+      timer(0,delay).pipe(
+        take(60/this.currentStage.cadence)
+        ).subscribe({
+          next: x => {
+            console.log("x: ",this.currentStage?.cadence, x);
+          }
+        });
+        this.onGoingTraining.next();
+
+
+    } while(!this.onGoingTraining.end)
+  
+    //this.trainingIterator$.next(this.onGoingTraining.first());
+
 
   }
 }
--- src/app/models/training-iterator.model.ts
+++ src/app/models/training-iterator.model.ts
@@ -1,31 +1,29 @@
 import {IStage, ITraining} from './training.model';
 
-export interface ITrainingIterator {
-  first(): IStage;
-  next(): IStage;
-  hasNext(): boolean;
+export interface ITrainingIterator <T> {
+  first(): void;  
+  next(): void;  
+  end: boolean;  
+  item: T;  
+  index: number;
 }
 
-export class RunTraining implements ITrainingIterator {
-  index: number = 0;
-  stages: IStage[];
-
-  constructor(training: ITraining) {
-    this.stages = training.stages;
-  }
-
-  first(): IStage {
-    this.index = 0;
+export class RunTraining<T> implements ITrainingIterator<T> {
+  index = 0;
+ 
+  constructor(public stages: T[]) { }  
+  
+  first(): void {    
+    this.index = 0;  
+  }  
+  next(): void {
+    this.index++;  
+  }  
+  get end(): boolean {    
+    return this.index >= this.stages.length;  
+  }  
+  get item(): T {    
     return this.stages[this.index];
   }
 
-  next(): IStage {
-    this.index++;
-    return this.stages[this.index];
-  }
-
-  hasNext(): boolean {
-    const lastIndex = this.stages.length - 1;
-    return this.index != lastIndex;
-  }
 }
