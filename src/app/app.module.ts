6640e8fafa7be71a32568d0422ca384e1fd489df
sraye <sylvie.raye@budget-box.com>
2023-09-25 09:07:51 +0200

    ROW-12 Revoir emplacement et responsive design des étapes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
 import {ReactiveFormsModule} from '@angular/forms';
 import {MatChipsModule} from '@angular/material/chips';
-import { StepComponent } from './components/run-training/step/step.component';
+import { StepComponent } from './components/trainings/step/step.component';
 
 @NgModule({
   declarations: [
