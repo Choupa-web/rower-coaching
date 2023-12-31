9a069068d4a310820e0423b10072e419e9746002
Choupa-web <persan78@hotmail.com>
2023-12-31 16:57:08 +0100

    ROW-13 ajouter les entrainements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { RunTrainingComponent } from './components/trainings/run-training/run-training.component';
 import { MatButtonModule } from '@angular/material/button';
-import {FlexLayoutModule} from '@ngbracket/ngx-layout';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatSelectModule} from '@angular/material/select';
-import {ReactiveFormsModule} from '@angular/forms';
-import {MatChipsModule} from '@angular/material/chips';
+import { FlexLayoutModule } from '@ngbracket/ngx-layout';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatChipsModule } from '@angular/material/chips';
 import { StepComponent } from './components/trainings/step/step.component';
-import {RemainingStepsComponent} from './components/trainings/remaining-steps/remaining-steps.component';
-import {RemainingStepComponent} from './components/trainings/remaining-step/remaining-step.component';
+import { RemainingStepsComponent } from './components/trainings/remaining-steps/remaining-steps.component';
+import { RemainingStepComponent } from './components/trainings/remaining-step/remaining-step.component';
 import { environment } from '../environments/environment';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
@@ -22,6 +23,7 @@ import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
   ],
   imports: [
     BrowserModule,
+    NoopAnimationsModule,
     AppRoutingModule,
     MatButtonModule,
     FlexLayoutModule,
--- src/app/models/trainings.model.ts
+++ src/app/models/trainings.model.ts
@@ -1,43 +1,420 @@
 import { ITraining } from "./training.model";
 
-export const looseWeightTraining : ITraining = {
-    label: "Perte de poids",
+export const footingEffectTraining : ITraining = {
+    label: "Effet footing 20mn",
     stages: [
       {
-        duration: {minutes: 0, seconds: 10},
+        duration: {minutes: 1, seconds: 30},
         cadence: 16,
         order: 0
       },
       {
         duration: {minutes: 0, seconds: 30},
-        cadence: 38,
+        cadence: 28,
         order: 1
       },
       {
-        duration: {minutes: 0, seconds: 10},
-        cadence: 16,
+        duration: {minutes: 1, seconds: 30},
+        cadence: 18,
         order: 2
       },
       {
         duration: {minutes: 0, seconds: 30},
-        cadence: 38,
+        cadence: 28,
         order: 3
+      },
+      {
+        duration: {minutes: 1, seconds: 30},
+        cadence: 20,
+        order: 4
+      },
+      {
+        duration: {minutes: 0, seconds: 30},
+        cadence: 28,
+        order: 5
+      },
+      {
+        duration: {minutes: 1, seconds: 30},
+        cadence: 22,
+        order: 6
+      },
+      {
+        duration: {minutes: 0, seconds: 30},
+        cadence: 28,
+        order: 7
+      },
+      {
+        duration: {minutes: 1, seconds: 30},
+        cadence: 24,
+        order: 8
+      },
+      {
+        duration: {minutes: 0, seconds: 30},
+        cadence: 28,
+        order: 9
+      },
+      {
+        duration: {minutes: 1, seconds: 30},
+        cadence: 16,
+        order: 10
+      },
+      {
+        duration: {minutes: 0, seconds: 30},
+        cadence: 28,
+        order: 11
+      },
+      {
+        duration: {minutes: 1, seconds: 30},
+        cadence: 18,
+        order: 12
+      },
+      {
+        duration: {minutes: 0, seconds: 30},
+        cadence: 28,
+        order: 13
+      },
+      {
+        duration: {minutes: 1, seconds: 30},
+        cadence: 20,
+        order: 14
+      },
+      {
+        duration: {minutes: 0, seconds: 30},
+        cadence: 28,
+        order: 15
+      },
+      {
+        duration: {minutes: 1, seconds: 30},
+        cadence: 22,
+        order: 16
+      },
+      {
+        duration: {minutes: 0, seconds: 30},
+        cadence: 28,
+        order: 17
+      },
+      {
+        duration: {minutes: 1, seconds: 30},
+        cadence: 24,
+        order: 18
+      },
+      {
+        duration: {minutes: 0, seconds: 30},
+        cadence: 28,
+        order: 19
       }
     ]
 }
 
-export const abdosTraining: ITraining = {
-    label: "Abdos",
-    stages: [
-      {
-        duration: {minutes: 0, seconds: 10},
-        cadence: 16,
-        order: 0
-      },
-      {
-        duration: {minutes: 0, seconds: 30},
-        cadence: 38,
-        order: 1
-      }
-    ]
+export const bruleGraissesTraining: ITraining = {
+  label: "Brûle graisse 23mn",
+  stages: [
+    {
+      duration: {minutes: 1, seconds: 0},
+      cadence: 24,
+      order: 0
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 18,
+      order: 1
+    },
+    {
+      duration: {minutes: 1, seconds: 0},
+      cadence: 26,
+      order: 2
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 18,
+      order: 3
+    },
+    {
+      duration: {minutes: 1, seconds: 0},
+      cadence: 28,
+      order: 4
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 18,
+      order: 5
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 30,
+      order: 6
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 18,
+      order: 7
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 32,
+      order: 8
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 18,
+      order: 9
+    },
+    {
+      duration: {minutes: 1, seconds: 0},
+      cadence: 24,
+      order: 10
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 18,
+      order: 11
+    },
+    {
+      duration: {minutes: 1, seconds: 0},
+      cadence: 26,
+      order: 12
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 18,
+      order: 13
+    },
+    {
+      duration: {minutes: 1, seconds: 0},
+      cadence: 28,
+      order: 14
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 18,
+      order: 15
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 30,
+      order: 16
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 18,
+      order: 17
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 32,
+      order: 18
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 18,
+      order: 19
+    },
+    {
+      duration: {minutes: 1, seconds: 0},
+      cadence: 24,
+      order: 20
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 18,
+      order: 21
+    },
+    {
+      duration: {minutes: 1, seconds: 0},
+      cadence: 26,
+      order: 22
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 18,
+      order: 23
+    },
+    {
+      duration: {minutes: 1, seconds: 0},
+      cadence: 28,
+      order: 24
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 18,
+      order: 25
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 30,
+      order: 26
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 18,
+      order: 27
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 32,
+      order: 28
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 18,
+      order: 29
+    }
+  ]
 }
+
+export const looseWeight1Training: ITraining = {
+  label: "Perdre du poids 24mn",
+  stages: [
+    {
+      duration: {minutes: 1, seconds: 0},
+      cadence: 24,
+      order: 0
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 18,
+      order: 1
+    },
+    {
+      duration: {minutes: 1, seconds: 0},
+      cadence: 26,
+      order: 2
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 18,
+      order: 3
+    },
+    {
+      duration: {minutes: 1, seconds: 0},
+      cadence: 28,
+      order: 4
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 18,
+      order: 5
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 30,
+      order: 6
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 18,
+      order: 7
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 32,
+      order: 8
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 18,
+      order: 9
+    },
+    {
+      duration: {minutes: 1, seconds: 0},
+      cadence: 24,
+      order: 10
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 18,
+      order: 11
+    },
+    {
+      duration: {minutes: 1, seconds: 0},
+      cadence: 26,
+      order: 12
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 18,
+      order: 13
+    },
+    {
+      duration: {minutes: 1, seconds: 0},
+      cadence: 28,
+      order: 14
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 18,
+      order: 15
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 30,
+      order: 16
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 18,
+      order: 17
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 32,
+      order: 18
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 18,
+      order: 19
+    },
+    {
+      duration: {minutes: 1, seconds: 0},
+      cadence: 24,
+      order: 20
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 18,
+      order: 21
+    },
+    {
+      duration: {minutes: 1, seconds: 0},
+      cadence: 26,
+      order: 22
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 18,
+      order: 23
+    },
+    {
+      duration: {minutes: 1, seconds: 0},
+      cadence: 28,
+      order: 24
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 18,
+      order: 25
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 30,
+      order: 26
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 18,
+      order: 27
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 32,
+      order: 28
+    },
+    {
+      duration: {minutes: 0, seconds: 30},
+      cadence: 18,
+      order: 29
+    }
+  ]
+}
+
